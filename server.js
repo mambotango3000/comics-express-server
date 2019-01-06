f300d66adb41dd3f76fa2ea3b07794e92ca58f77
mambotango3000 <mambotango3000@gmail.com>
2019-01-06 16:06:55 -0500

    updating to use remote db on mLab when in production

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var db = mongoose.connect('mongodb://localhost/comicsApi');
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if(env === 'development') {
+    console.log('developmnet');
+    mongoose.connect('mongodb://localhost/comicsApi');
+  } else {
+      console.log('production');
+    mongoose.connect('mongodb://comicsApp:comicsAppPW1@ds145325.mlab.com:45325/comicsapi');
+  }
 
 var Comic = require('./models/comicModel');
 
 var app = express();
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -17,7 +28,9 @@ comicRouter = require('./routes/comic-routes')(Comic);
 
 app.use('/', comicRouter);
 
-app.listen(8000);
-console.log('Listening on port ' + 8000 + '...');
+var port = process.env.PORT || 8000;
+
+app.listen(port);
+console.log('Listening on port ' + port + '...');
 
 module.exports = app;
